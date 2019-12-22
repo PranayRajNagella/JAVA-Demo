c4edb2f4bbe6df796896caf3f70e0aa27cfbbd17
Pranay Raj <pranayrajnagella@gmail.com>
2019-12-22 23:40:11 +0530

    hhh

--- DemoGit/src/com/sample/Test.java
+++ DemoGit/src/com/sample/Test.java
@@ -6,7 +6,14 @@ public class Test {
 		System.out.println("Hello world");
 		System.out.println("");
 		System.out.println("Hello world i have come");
-
+		Test.add(2,"Hello");
+		
+	}
+	
+	static void add(int s, String y)
+	{
+		System.out.println(s+"Paremter i have passed"+y);
+		
 	}
 
 }
